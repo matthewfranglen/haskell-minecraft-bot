5a6ffffd35f1d8c19450202061fe7f4fe66036b0
Matthew Franglen <matthew@franglen.org>
2016-06-23 22:40:20 +0100

    :hibiscus: Successfully connect to minecraft server
    I didn't include sensitive info at least ;-)
    
        [22:34:46] [Server thread/INFO]: Preparing spawn area: 26%
        [22:34:47] [Server thread/INFO]: Done (2.369s)! For help, type "help" or "?"
        [22:35:03] [Server thread/INFO]: bot[/192.168.1.53:58420] logged in with entity id 202 at (-21.51583519280641, 63.001, 178.5)
        [22:35:03] [Server thread/INFO]: bot joined the game
    
    ... Me pressing Ctrl-C ...
    
        [22:35:08] [Server thread/INFO]: bot lost connection: TextComponent{text='Disconnected', siblings=[], style=Style{hasParent=false, color=null, bold=null, italic=null, underlined=null, obfuscated=null, clickEvent=null, hoverEvent=null, insertion=null}}
        [22:35:08] [Server thread/INFO]: bot left the game

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,17 @@
 module Main where
 
+import FFI
+
+data Sys
+
 main :: Fay ()
 main = do
-    putStrLn "Hello, World!"
+    mineflayer <- require' "mineflayer"
+    createBot mineflayer "192.168.1.50" 25565 "bot"
+
+require' :: String -> Fay Sys
+require' = ffi "require(%1)"
+
+createBot :: Sys -> String -> Int -> String -> Fay ()
+createBot = ffi "%1.createBot({ host: %2, port: %3, username: %4 })"
+
